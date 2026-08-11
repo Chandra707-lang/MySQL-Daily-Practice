7a34a1c895a3bafbee77fe72d56c736cc67e071f
Chandra707-lang <23f2000530@ds.study.iitm.ac.in>
2026-08-11 21:22:28 +0530

    Add files via upload

---
+++ rank,dense rank function,row number.sql
@@ -0,0 +1,21 @@
+create database windows10db;
+ use windows10db;
+
+ 
+ select * from employee_performance;
+ 
+ select employee_id,employee_name,location,sales_amount,salary,row_number() over(partition by location) from employee_performance;
+ 
+ select employee_id,employee_name,location,sales_amount,salary,rank() over(partition by sales_amount) from employee_performance;
+ 
+  select employee_id,employee_name,location,sales_amount,salary,rank() over(order  by sales_amount) from employee_performance;
+ 
+  select employee_id,employee_name,location,sales_amount,salary,rank() over(order by salary ) from employee_performance;
+  
+select employee_id,employee_name,location,sales_amount,salary,rank() over(partition by location order by salary desc) from employee_performance;
+    
+  select employee_id,employee_name,location,sales_amount,salary,dense_rank() over(partition by location order by salary desc) from employee_performance;
+  
+  select employee_id,employee_name,location,sales_amount,salary,rank() over(partition by location order by sales_amount desc) from employee_performance;
+  
+  
